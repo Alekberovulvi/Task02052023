3004c9a5224aa817a87707bf608e3a93f4981dc8
Alekberovulvi <ulvina@code.edu.az>
2023-05-02 23:13:47 +0400

    task02052023

---
+++ Task 02.05.2023/index.py
@@ -0,0 +1,47 @@
+# num1=input("Reqem daxil edin:  ") 
+# num2=input("ikinci reqemi daxil edin:  ")
+# print(num1>num2)
+# print(num1<num2)
+# print(num1>=num2)
+# print(num1<=num2)
+# print(not num1 == num2)
+# print(num1 != num2)
+# print(id(num1))
+# print(id(num2))
+
+# num3 = 70
+# num4 = 70.0
+
+# print(id(num3) ==id(num4))
+
+
+# number1 = 30
+# number2 = 10
+
+# print(str(id(number1))[:4] == str(id(number2))[:4])
+
+
+
+
+
+
+
+
+
+# name1 = input("adiniz: ")
+# name2 = input("soyadiniz is: ")
+
+# print((name1[0]==name2[0]) or name1[-1] == name2[-1] )
+# print((len(name1)==len(name2) and (id(name1) >= id(name2)) or (name1==name2 and id(name1)==id(name2))))
+
+
+
+
+
+
+# a,b = "Coders ", "Azerbaijan"
+# print(a)
+# print(b)
+# a += b
+
+# print(a)
